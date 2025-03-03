f1feaef46604a8572151c6005cddb3c3e5bde0a3
AbeDevSaga <2000amandanm@gmail.com>
2025-03-03 23:05:23 +0300

    All origins are allowed

--- server.js
+++ server.js
@@ -9,8 +9,12 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
-
+// app.use(cors());
+app.use(cors({
+    origin: '*', 
+    methods: 'GET,POST,PUT,DELETE', 
+    allowedHeaders: 'Content-Type,Authorization'
+}));
 connectDB();
 
 // app.use((req, res, next) => {
